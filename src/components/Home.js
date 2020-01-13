41d3d3e3d5b8edde568c6396bcc4149e88f0a6a7
TSN Student <student@ulem.org>
2020-01-13 15:49:42 -0500

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>THE WORLD'S LAST HOPE.</h1>
+      <div className="choice">
+        <div className="box-2">
+          <div className="btn btn-two">
+            <span>ENTER</span>
+          </div>
+        </div>
+        <div className="box-2">
+          <div className="btn btn-two">
+            <span>CONNECTION</span>
+          </div>
+        </div>
+      </div>
+      <p>
+        This is a information page so you can get to know the man behind the
+        smile.
+      </p>
+    </div>
+  );
+}
