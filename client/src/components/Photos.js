5446c983ff78a25806fa996827170fe3c886c814
TSN Student <student@ulem.org>
2020-02-03 13:40:28 -0500

    sigh

--- client/src/components/Photos.js
+++ client/src/components/Photos.js
@@ -4,19 +4,19 @@ import Footer from "./Footer";
 import $ from "jquery";
 import { Link } from "react-router-dom";
 import a from "./photos/0.jpg";
-import b from "./photos/1.jpg";
-import c from "./photos/2.jpg";
-import d from "./photos/3.jpg";
-import e from "./photos/4.jpg";
-import f from "./photos/5.jpg";
-import g from "./photos/6.jpg";
-import h from "./photos/7.jpg";
-import i from "./photos/8.jpg";
-import j from "./photos/9.jpg";
-import k from "./photos/10.jpg";
-import l from "./photos/11.jpg";
-import m from "./photos/blue.jpg";
-import n from "./photos/fly.jpg";
+import b from "../photos/1.jpg";
+import c from "../photos/2.jpg";
+import d from "../photos/3.jpg";
+import e from "../photos/4.jpg";
+import f from "../photos/5.jpg";
+import g from "../photos/6.jpg";
+import h from "../photos/7.jpg";
+import i from "../photos/8.jpg";
+import j from "../photos/9.jpg";
+import k from "../photos/10.jpg";
+import l from "../photos/11.jpg";
+import m from "../photos/blue.jpg";
+import n from "../photos/fly.jpg";
 
 export default function Photos() {
   return (
