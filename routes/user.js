437838a6480716e70ad9f8bc031fd67baafabb44
TSN Student <student@ulem.org>
2020-02-05 10:44:19 -0500

    adding mailgun

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const mailgun = require("mailgun-js");
 
 router.post("/", async (req, res) => {
   const user = req.body;
-  const data = await User.create(user);
-  res.json(data);
+  const API_KEY = process.env.MAILGUN_API_KEY || "test";
+  const DOMAIN = process.env.MAILGUN_DOMAIN || "test";
+  const mg = mailgun({ api_key: API_KEY, domain: DOMAIN });
+  const data = {
+    from: user.email,
+    to: "william.butler0423@gmail.com",
+    subject: user.subject,
+    text: user.message
+  };
+  mg.messages().send(data, function(error, body) {
+    if (error) throw error;
+    console.log(body);
+    const data = await User.create(user);
+    res.json(data);
+  });
 });
 
 router.get("/", async (req, res) => {
