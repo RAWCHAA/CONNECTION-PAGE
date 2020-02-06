672154f0c8a18995d89376503ed0bdf309d0e153
TSN Student <student@ulem.org>
2020-02-06 11:54:08 -0500

    tweeks

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,19 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-      <h1>THE WORLD'S LAST HOPE.</h1>
+      <div id="container-anim">
+        <div id="flip">
+          <div>
+            <div>Rawchaa Ahraya</div>
+          </div>
+          <div>
+            <div>Officer of 500</div>
+          </div>
+          <div>
+            <div>Priest Of The I.S.U.P.K.</div>
+          </div>
+        </div>
+      </div>
       <div className="choice">
         <div className="box-2">
           <div className="btn btn-two">
@@ -17,10 +29,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <p className="open">
-        This is a information page so you can get to know the man behind the
-        smile.
-      </p>
+      <p className="open">The Lion's Spirit Lives</p>
     </div>
   );
 }
